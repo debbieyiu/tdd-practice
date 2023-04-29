b1d8fdbe66fcbc030294ba61c4d599ef1fb8e3d5
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-04-29 18:06:28 +0800

    [refactor] refactor test case: extract SUT in setup and extract method-"AssertShowResultShouldReturn"

--- SibalaGame/GameTest.cs
+++ SibalaGame/GameTest.cs
@@ -6,12 +6,24 @@ namespace SibalaGame
     [TestFixture]
     public class GameTest
     {
+        private Game _target;
+
+        [SetUp]
+        public void @Setup()
+        {
+            _target = new Game();
+        }
+
         [Test]
         public void A01_BothAllOfAKind()
         {
-            var target = new Game();
-            var actual = target.ShowResult("Black: 5 5 5 5  White: 2 2 2 2");
-            actual.Should().Be("Black win. - with all of a kind: 5");
+            AssertShowResultShouldReturn("Black: 5 5 5 5  White: 2 2 2 2", "Black win. - with all of a kind: 5");
+        }
+
+        private void AssertShowResultShouldReturn(string input, string expected)
+        {
+            var actual = _target.ShowResult(input);
+            actual.Should().Be(expected);
         }
     }
 }
