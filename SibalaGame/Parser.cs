0babcd0a3b845aa67b5f7d6f0fc9e29f60a0e712
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-04-29 23:14:59 +0800

    [test] (red) ParserTest.cs: add test case "A01_Parse_ParsePlayerName", to design class-Parser,Player and function-Parse()
    baby step: expected to assert PlayerName first

---
+++ SibalaGame/Parser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SibalaGame
+{
+    public class Parser
+    {
+        public List<Player> Parse(string blackWhite)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
