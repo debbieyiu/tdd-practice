63f62502bced0b849a707e7a1c427d621b4a2444
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-01 16:40:32 +0800

    [refactor] Parser.cs: parse input parameter to get player1's dices to replace hard-code.

--- SibalaGame/Parser.cs
+++ SibalaGame/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SibalaGame
 {
@@ -10,6 +11,10 @@ namespace SibalaGame
             var playerSections = input.Split("  ");
             var player1AndDices = playerSections[0].Split(":", StringSplitOptions.RemoveEmptyEntries);
             var player1Name = player1AndDices[0];
+            var player1Dices = player1AndDices[1]
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(str => new Dice { Value = int.Parse(str), Output = str })
+                .ToList();
 
             var player2AndDices = playerSections[1].Split(":", StringSplitOptions.RemoveEmptyEntries);
             var player2Name = player2AndDices[0];
@@ -19,13 +24,7 @@ namespace SibalaGame
                 new Player
                 {
                     Name = player1Name,
-                    Dices = new List<Dice>
-                    {
-                        new Dice { Value = 5, Output = "5" },
-                        new Dice { Value = 5, Output = "5" },
-                        new Dice { Value = 5, Output = "5" },
-                        new Dice { Value = 5, Output = "5" }
-                    }
+                    Dices = player1Dices
                 },
                 new Player { Name = player2Name }
             };
