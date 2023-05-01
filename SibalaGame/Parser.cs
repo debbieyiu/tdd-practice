385bbcdb666cf3e105b532d93b1b0dff383629ad
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-01 16:51:14 +0800

    [refactor] Parser.cs: extract method "GetPlayer()" and re-arrange code.

--- SibalaGame/Parser.cs
+++ SibalaGame/Parser.cs
@@ -9,34 +9,41 @@ namespace SibalaGame
         public List<Player> Parse(string input)
         {
             var playerSections = input.Split("  ");
-            var player1AndDices = playerSections[0].Split(":", StringSplitOptions.RemoveEmptyEntries);
-            var player1Name = player1AndDices[0];
-            var player1Dices = player1AndDices[1]
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(str => new Dice { Value = int.Parse(str), Output = str })
-                .ToList();
+            var player1 = GetPlayer(playerSections[0]);
 
-            var player2AndDices = playerSections[1].Split(":", StringSplitOptions.RemoveEmptyEntries);
+            var player2Section = playerSections[1];
+            var player2AndDices = player2Section.Split(":", StringSplitOptions.RemoveEmptyEntries);
             var player2Name = player2AndDices[0];
             var player2Dices = player2AndDices[1]
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(str => new Dice { Value = int.Parse(str), Output = str })
                 .ToList();
-
-            var player1 = new Player
-            {
-                Name = player1Name,
-                Dices = player1Dices
-            };
             var player2 = new Player
             {
                 Name = player2Name,
                 Dices = player2Dices
             };
+
             return new List<Player>
             {
                 player1,
                 player2
+            };
+        }
+
+        private static Player GetPlayer(string playerSection)
+        {
+            var playerAndDices = playerSection.Split(":", StringSplitOptions.RemoveEmptyEntries);
+            var name = playerAndDices[0];
+            var dices = playerAndDices[1]
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(str => new Dice { Value = int.Parse(str), Output = str })
+                .ToList();
+
+            return new Player
+            {
+                Name = name,
+                Dices = dices
             };
         }
     }
