ce681e60ffb327277283070fa1f94b52820a5b09
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-01 16:03:42 +0800

    [implement] (green) hard-code return.

--- SibalaGame/Parser.cs
+++ SibalaGame/Parser.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections.Generic;
-using NUnit.Framework;
 
 namespace SibalaGame
 {
     public class Parser
     {
-        public List<Player> Parse(string blackWhite)
+        public List<Player> Parse(string input)
         {
-            throw new System.NotImplementedException();
+            return new List<Player>
+            {
+                new Player { Name = "Black" },
+                new Player { Name = "White" }
+            };
         }
     }
 }
