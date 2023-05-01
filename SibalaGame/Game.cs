aa20e588dec6c5f14f06d428ba0043dd8dd0664b
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-01 18:00:37 +0800

    [refactor] Game.cs: use compareResult to replace compare two dice value.
    compareResult rule:
    1. >0: player1 win.
    2. <0: player2 win.
    3. =0: Tie.

--- SibalaGame/Game.cs
+++ SibalaGame/Game.cs
@@ -12,14 +12,16 @@ namespace SibalaGame
             var player1Dice = players[0].Dices.First();
             var player2Dice = players[1].Dices.First();
 
-            if (player1Dice.Value == player2Dice.Value)
+            var compareResult = player1Dice.Value - player2Dice.Value;
+
+            if (compareResult == 0)
             {
                 return "Tie.";
             }
 
             string winnerPlayer;
             string winnerOutput;
-            if (player1Dice.Value > player2Dice.Value)
+            if (compareResult > 0)
             {
                 winnerPlayer = players[0].Name;
                 winnerOutput = player1Dice.Output;
