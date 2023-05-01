a2bda9968696d5020ca9257df36f1687ea0c479c
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-01 17:26:30 +0800

    [implement] (green) Game.cs: compare dice value to determine which one player wins.

--- SibalaGame/Game.cs
+++ SibalaGame/Game.cs
@@ -9,10 +9,23 @@ namespace SibalaGame
             var parser = new Parser();
             var players = parser.Parse(input);
 
-            var winnerPlayer = players[0].Name;
-            var winnerCategory = "all of a kind";
-            var winnerOutput = players[0].Dices.First().Output;
-            return $"{winnerPlayer} win. - with {winnerCategory}: {winnerOutput}";
+            var player1Dice = players[0].Dices.First();
+            var player2Dice = players[1].Dices.First();
+
+            if (player1Dice.Value > player2Dice.Value)
+            {
+                var winnerPlayer = players[0].Name;
+                var winnerCategory = "all of a kind";
+                var winnerOutput = player1Dice.Output;
+                return $"{winnerPlayer} win. - with {winnerCategory}: {winnerOutput}";
+            }
+            else
+            {
+                var winnerPlayer = players[1].Name;
+                var winnerCategory = "all of a kind";
+                var winnerOutput = player2Dice.Output;
+                return $"{winnerPlayer} win. - with {winnerCategory}: {winnerOutput}";
+            }
         }
     }
 }
