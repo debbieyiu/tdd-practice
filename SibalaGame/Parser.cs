25771665161c71eadc3a74ad7ade62faf8e63a13
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-01 16:06:52 +0800

    [refactor] Parser.cs: parse input parameter to get player1's name to replace hard-code.

--- SibalaGame/Parser.cs
+++ SibalaGame/Parser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SibalaGame
 {
@@ -6,9 +7,13 @@ namespace SibalaGame
     {
         public List<Player> Parse(string input)
         {
+            var playerSections = input.Split("  ");
+            var playerAndDices = playerSections[0].Split(":", StringSplitOptions.RemoveEmptyEntries);
+            var player1Name = playerAndDices[0];
+
             return new List<Player>
             {
-                new Player { Name = "Black" },
+                new Player { Name = player1Name },
                 new Player { Name = "White" }
             };
         }
