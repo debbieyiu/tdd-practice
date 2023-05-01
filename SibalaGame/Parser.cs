b483a2adc9baddc4c99c9547dae067c2645fd2a5
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-01 16:58:15 +0800

    [refactor] Parser.cs: inline variable player1 and player2

--- SibalaGame/Parser.cs
+++ SibalaGame/Parser.cs
@@ -9,13 +9,11 @@ namespace SibalaGame
         public List<Player> Parse(string input)
         {
             var playerSections = input.Split("  ");
-            var player1 = GetPlayer(playerSections[0]);
-            var player2 = GetPlayer(playerSections[1]);
 
             return new List<Player>
             {
-                player1,
-                player2
+                GetPlayer(playerSections[0]),
+                GetPlayer(playerSections[1])
             };
         }
 
