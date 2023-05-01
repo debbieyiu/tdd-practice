03d63299ad46d00acea8d08765523f88222e6769
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-01 17:11:15 +0800

    [refactor] Game.cs: use Parser.Parse() to replace winnerPlayer from player1's name.

--- SibalaGame/Game.cs
+++ SibalaGame/Game.cs
@@ -4,9 +4,10 @@
     {
         public string ShowResult(string input)
         {
-            // var parser = new Parser();
-            // var players = parser.Parse(input);
-            var winnerPlayer = "Black";
+            var parser = new Parser();
+            var players = parser.Parse(input);
+
+            var winnerPlayer = players[0].Name;
             var winnerCategory = "all of a kind";
             var winnerOutput = "5";
             return $"{winnerPlayer} win. - with {winnerCategory}: {winnerOutput}";
