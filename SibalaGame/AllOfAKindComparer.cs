7a01712d6f2f9f7e6314d147e6e40a9d94760cf8
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-03 23:24:29 +0800

    [refactor] Game.cs: WinnerOutput change get from allOfAKindComparer.WinnerOutput replace of out parameter.

--- SibalaGame/AllOfAKindComparer.cs
+++ SibalaGame/AllOfAKindComparer.cs
@@ -4,13 +4,14 @@
     {
         public string WinnerCategoryName => "all of a kind";
 
-        public int CompareResult(Dice player1Dice, Dice player2Dice, out string winnerOutput)
+        public string WinnerOutput { get; private set; }
+
+        public int CompareResult(Dice player1Dice, Dice player2Dice)
         {
-            winnerOutput = string.Empty;
             var compareResult = player1Dice.Value - player2Dice.Value;
             if (compareResult != 0)
             {
-                winnerOutput = compareResult > 0 ? player1Dice.Output : player2Dice.Output;
+                WinnerOutput = compareResult > 0 ? player1Dice.Output : player2Dice.Output;
             }
 
             return compareResult;
--- SibalaGame/Game.cs
+++ SibalaGame/Game.cs
@@ -13,12 +13,12 @@ namespace SibalaGame
             var player2Dice = players[1].Dices.First();
 
             var allOfAKindComparer = new AllOfAKindComparer();
-            var compareResult = allOfAKindComparer.CompareResult(player1Dice, player2Dice, out var winnerOutput);
+            var compareResult = allOfAKindComparer.CompareResult(player1Dice, player2Dice);
             if (compareResult != 0)
             {
                 var winnerPlayer = compareResult > 0 ? players[0].Name : players[1].Name;
                 var winnerCategory = allOfAKindComparer.WinnerCategoryName;
-                return $"{winnerPlayer} win. - with {winnerCategory}: {winnerOutput}";
+                return $"{winnerPlayer} win. - with {winnerCategory}: {allOfAKindComparer.WinnerOutput}";
             }
 
             return "Tie.";
