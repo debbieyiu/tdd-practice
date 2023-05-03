01a1161211f86373604bdb54e6cabaaa6d944acf
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-05-03 23:09:57 +0800

    [refactor] Game.cs: Compare() to extract class - AllOfAKindComparer

---
+++ SibalaGame/AllOfAKindComparer.cs
@@ -0,0 +1,17 @@
+﻿namespace SibalaGame
+{
+    public class AllOfAKindComparer
+    {
+        public int CompareResult(Dice player1Dice, Dice player2Dice, out string winnerOutput)
+        {
+            winnerOutput = string.Empty;
+            var compareResult = player1Dice.Value - player2Dice.Value;
+            if (compareResult != 0)
+            {
+                winnerOutput = compareResult > 0 ? player1Dice.Output : player2Dice.Output;
+            }
+
+            return compareResult;
+        }
+    }
+}
