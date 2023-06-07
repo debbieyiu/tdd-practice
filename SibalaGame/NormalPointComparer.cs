c24729cd9a6e2ae203c87c87f65dd48886dc1657
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-06-07 19:32:52 +0800

    [implement] (green) NormalPointComparer.cs: compare max dice value of calDices when two normal point is equals.

--- SibalaGame/NormalPointComparer.cs
+++ SibalaGame/NormalPointComparer.cs
@@ -10,21 +10,32 @@ namespace SibalaGame
 
         public int Compare(List<Dice> player1Dices, List<Dice> player2Dices)
         {
-            var diceValuePlayer1 = CalculateNormalPoint(player1Dices);
-            var diceValuePlayer2 = CalculateNormalPoint(player2Dices);
+            var diceValuePlayer1 = CalculateNormalPointDices(player1Dices);
+            var diceValuePlayer2 = CalculateNormalPointDices(player2Dices);
+            var dicesPointPlayer1 = diceValuePlayer1.Sum(dice => dice.Value);
+            var dicesPointPlayer2 = diceValuePlayer2.Sum(dice => dice.Value);
 
-            var compareResult2 = diceValuePlayer1 - diceValuePlayer2;
-            WinnerOutput = compareResult2 > 0 ? diceValuePlayer1.ToString() : diceValuePlayer2.ToString();
-            return compareResult2;
+            var compareResult = dicesPointPlayer1 - dicesPointPlayer2;
+            if (compareResult == 0)
+            {
+                compareResult = diceValuePlayer1.Max(dice => dice.Value) - diceValuePlayer2.Max(dice => dice.Value);
+            }
+
+            if (compareResult != 0)
+            {
+                WinnerOutput = compareResult > 0 ? dicesPointPlayer1.ToString() : dicesPointPlayer2.ToString();
+            }
+
+            return compareResult;
         }
 
-        private static int CalculateNormalPoint(List<Dice> dices)
+        private static List<Dice> CalculateNormalPointDices(List<Dice> dices)
         {
             var minPairs = dices.GroupBy(dice => dice.Value)
                 .OrderBy(grouping => grouping.Key)
                 .First(grouping => grouping.Count() == 2)
                 .ToList();
-            return dices.Except(minPairs).Sum(dice => dice.Value);
+            return dices.Except(minPairs).ToList();
         }
     }
 }
