2eec9929e93c80b4d0df2d3a17309284fb35ab73
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-07-19 21:50:45 +0800

    extra method Compare()

--- SibalaGame/Game.cs
+++ SibalaGame/Game.cs
@@ -13,6 +13,19 @@ namespace SibalaGame
             var player2Dices = players[1].Dices;
 
             IComparer comparer;
+            comparer = Compare(player1Dices, player2Dices, out var compareResult);
+            if (compareResult != 0)
+            {
+                var winnerPlayer = compareResult > 0 ? players[0].Name : players[1].Name;
+                return $"{winnerPlayer} win. - with {comparer.WinnerCategory.Description}";
+            }
+
+            return "Tie.";
+        }
+
+        private IComparer Compare(Dices player1Dices, Dices player2Dices, out int compareResult)
+        {
+            IComparer comparer;
             if (player1Dices.GetCategory().Type != player2Dices.GetCategory().Type)
             {
                 comparer = new DifferentCategoryComparer();
@@ -29,14 +42,8 @@ namespace SibalaGame
                 comparer = sameCategoryComparerLookup[player1Dices.GetCategory().Type];
             }
 
-            var compareResult = comparer.Compare(player1Dices, player2Dices);
-            if (compareResult != 0)
-            {
-                var winnerPlayer = compareResult > 0 ? players[0].Name : players[1].Name;
-                return $"{winnerPlayer} win. - with {comparer.WinnerCategory.Description}";
-            }
-
-            return "Tie.";
+            compareResult = comparer.Compare(player1Dices, player2Dices);
+            return comparer;
         }
     }
 }
