dc5c6269f4ff6e35b64f37ce1e5714c8ff19d06a
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-07-19 22:03:35 +0800

    extra class DicesComparer and inherit IComparer

---
+++ SibalaGame/DicesComparer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace SibalaGame
+{
+    public class DicesComparer : IComparer
+    {
+        public Category WinnerCategory { get; set; }
+
+        public int Compare(Dices dices1, Dices dices2)
+        {
+            IComparer comparer;
+            if (dices1.GetCategory().Type != dices2.GetCategory().Type)
+            {
+                comparer = new DifferentCategoryComparer();
+            }
+            else
+            {
+                var sameCategoryComparerLookup = new Dictionary<CategoryType, IComparer>
+                {
+                    { CategoryType.AllOfAKind, new AllOfAKindComparer() },
+                    { CategoryType.NormalPoint, new NormalPointComparer() },
+                    { CategoryType.NoPoint, new NoPointComparer() },
+                };
+
+                comparer = sameCategoryComparerLookup[dices1.GetCategory().Type];
+            }
+
+            var compareResult = comparer.Compare(dices1, dices2);
+            WinnerCategory = compareResult > 0 ? dices1.GetCategory() : dices2.GetCategory();
+            return compareResult;
+        }
+    }
+}
--- SibalaGame/Game.cs
+++ SibalaGame/Game.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 namespace SibalaGame
 {
@@ -12,8 +11,8 @@ namespace SibalaGame
             var player1Dices = players[0].Dices;
             var player2Dices = players[1].Dices;
 
-            IComparer comparer;
-            comparer = Compare(player1Dices, player2Dices, out var compareResult);
+            var comparer = new DicesComparer();
+            var compareResult = comparer.Compare(player1Dices, player2Dices);
             if (compareResult != 0)
             {
                 var winnerPlayer = compareResult > 0 ? players[0].Name : players[1].Name;
@@ -21,29 +20,6 @@ namespace SibalaGame
             }
 
             return "Tie.";
-        }
-
-        private IComparer Compare(Dices player1Dices, Dices player2Dices, out int compareResult)
-        {
-            IComparer comparer;
-            if (player1Dices.GetCategory().Type != player2Dices.GetCategory().Type)
-            {
-                comparer = new DifferentCategoryComparer();
-            }
-            else
-            {
-                var sameCategoryComparerLookup = new Dictionary<CategoryType, IComparer>
-                {
-                    { CategoryType.AllOfAKind, new AllOfAKindComparer() },
-                    { CategoryType.NormalPoint, new NormalPointComparer() },
-                    { CategoryType.NoPoint, new NoPointComparer() },
-                };
-
-                comparer = sameCategoryComparerLookup[player1Dices.GetCategory().Type];
-            }
-
-            compareResult = comparer.Compare(player1Dices, player2Dices);
-            return comparer;
         }
     }
 }
