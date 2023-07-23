630144ae59c425bc5d505929de96f34bab417640
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-07-23 21:17:52 +0800

    [test] (red) add test case - both all of a kind - Black: 5 5 5 5  White: 2 2 2 2, to design class-Game and function-ShowResult()

---
+++ SibalaGame/SibalaGameTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace SibalaGame
+{
+    [TestFixture]
+    public class SibalaGameTest
+    {
+        private Game _target;
+
+        [SetUp]
+        public void @Setup()
+        {
+            _target = new Game();
+        }
+
+        [Test]
+        public void A01_Both_AllOfAKind()
+        {
+            var input = "Black: 5 5 5 5  White: 2 2 2 2";
+            var actual = _target.ShowResult(input);
+            actual.Should().Be("Black win with all of a kind: 5");
+        }
+    }
+}
