23d8118e41919a488e9133580e36ec88a883833c
Debbie Yiu <debbie.yiu@imail.aircast.biz>
2023-09-09 17:17:07 +0800

    [refactor] Game.cs: Re-arrange code to prepare extract method - Compare(dices1, dices2)

--- SibalaGame/Game.cs
+++ SibalaGame/Game.cs
@@ -8,15 +8,21 @@ namespace SibalaGame
         {
             var parser = new Parser();
             var players = parser.Parse(input);
-            var dice1 = players[0].Dices.First();
-            var dice2 = players[1].Dices.First();
+            var dices1 = players[0].Dices;
+            var dices2 = players[1].Dices;
 
-            var compareResult = dice1.Value - dice2.Value;
+            var compareResult = dices1.First().Value - dices2.First().Value;
+            // prepare to extract method: Compare(dices1, dices2)
+            string winnerOutput = null;
+            if (compareResult != 0)
+            {
+                winnerOutput = compareResult > 0 ? dices1.First().Output : dices2.First().Output;
+            }
+
             if (compareResult != 0)
             {
                 var winnerPlayer = compareResult > 0 ? players[0].Name : players[1].Name;
                 var winnerCategory = "all of a kind";
-                var winnerOutput = compareResult > 0 ? dice1.Output : dice2.Output;
                 return $"{winnerPlayer} win with {winnerCategory}: {winnerOutput}";
             }
 
