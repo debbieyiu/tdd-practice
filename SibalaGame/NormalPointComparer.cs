7d6726a5bc62288581eeb4fb45f5966d7f177567
Debbie Yiu <debbie.yiu.0219@gmail.com>
2023-10-22 19:13:35 +0800

    [implement] (green) compare max dice value when both normal point compare value is same

--- SibalaGame/NormalPointComparer.cs
+++ SibalaGame/NormalPointComparer.cs
@@ -1,10 +1,17 @@
-﻿namespace SibalaGame
+﻿using System.Linq;
+
+namespace SibalaGame
 {
     public class NormalPointComparer
     {
         public int Compare(Dices dices1, Dices dices2)
         {
             var compareResult = dices1.GetCompareValue() - dices2.GetCompareValue();
+            if (compareResult == 0)
+            {
+                compareResult = dices1.Max(dice => dice.Value) - dices2.Max(dice => dice.Value);
+            }
+
             WinnerOutputDisplay = compareResult > 0 ? dices1.GetCompareValue().ToString() : dices2.GetCompareValue().ToString();
             return compareResult;
         }
