19242a76a7e7606d50f303f7d94650423da55a9a
Debbie Yiu <debbie.yiu.0219@gmail.com>
2023-10-29 23:07:32 +0800

    [refactor] DifferentCategoryComparer.Compare result coming from categorytype value difference

--- SibalaGame/CategoryType.cs
+++ SibalaGame/CategoryType.cs
@@ -1,8 +1,12 @@
-﻿namespace SibalaGame
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SibalaGame
 {
     public enum CategoryType
     {
+        [Display(Name = "normal point")]
         NormalPoint,
+        [Display(Name = "all of a kind")]
         AllOfAKind
     }
 }
