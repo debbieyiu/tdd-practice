fe2eddc7f03b9085be5612288e8a9a4657460336
Debbie Yiu <debbie.yiu.0219@gmail.com>
2023-11-05 19:23:31 +0800

    [implement] compare CategoryType to get compare result

--- SibalaGame/DifferentCategoryComparer.cs
+++ SibalaGame/DifferentCategoryComparer.cs
@@ -5,7 +5,7 @@
         public int Compare(Dices dices1, Dices dices2)
         {
             WinnerOutputDisplay = "5";
-            return 1;
+            return dices1.CategoryType - dices2.CategoryType;
         }
 
         public string WinnerCategoryDisplay => "all of a kind";
