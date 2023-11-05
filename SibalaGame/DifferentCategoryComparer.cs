c4107dc81594adbdef7bee60174995527ed013a3
Debbie Yiu <debbie.yiu.0219@gmail.com>
2023-11-05 20:41:03 +0800

    [implement] replace WinnerCategoryDisplay with GetDisplayName()

--- SibalaGame/DifferentCategoryComparer.cs
+++ SibalaGame/DifferentCategoryComparer.cs
@@ -1,14 +1,32 @@
-﻿namespace SibalaGame
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SibalaGame
 {
     public class DifferentCategoryComparer : ICompare
     {
         public int Compare(Dices dices1, Dices dices2)
         {
             WinnerOutputDisplay = "5";
-            return dices1.CategoryType - dices2.CategoryType;
+            var compareResult = dices1.CategoryType - dices2.CategoryType;
+            if (compareResult != 0)
+            {
+                WinnerCategoryDisplay = compareResult > 0
+                    ? GetDisplayName(dices1.CategoryType)
+                    : GetDisplayName(dices2.CategoryType);
+            }
+            return compareResult;
         }
 
-        public string WinnerCategoryDisplay => "all of a kind";
+        private string GetDisplayName(CategoryType category)
+        {
+            var type = typeof(CategoryType);
+            var memberInfo = type.GetMember(category.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(DisplayAttribute), false);
+            return ((DisplayAttribute)attributes[0]).Name;
+        }
+
+        public string WinnerCategoryDisplay { get; set; }
+
         public string WinnerOutputDisplay { get; set; }
     }
 }
